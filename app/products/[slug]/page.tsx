36a270f1e0f36444af7ca4ba9ad0b55eea04136d
Reddy Prasad D <dev.drprasad@aim.com>
2024-04-13 18:22:32 +0530

    📝 Add markdown-ed description to detail page

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,8 +1,8 @@
-import { Product } from "@/app/types";
 import React from "react";
-
 import ProductDetailCard from "./ProductDetailCard";
 import { getBackendURLPrefix } from "@/backend/utils";
+import { Product } from "@/app/types";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
 interface URLParams {
   slug: string;
@@ -26,8 +26,27 @@ const ProductDetail = async (props: Props) => {
   const product = await getProductBySlug(slug);
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <ProductDetailCard product={product} />
+
+      <div className="p-4 rounded-lg border border-default-200">
+        <MDXRemote
+          components={{
+            a: (props) => props.children,
+            h3: (props) => (
+              <h3 className="text-lg font-bold mb-4 mt-8">{props.children}</h3>
+            ),
+            h4: (props) => (
+              <h4 className="text-md font-bold mb-4 mt-8">{props.children}</h4>
+            ),
+            ul: (props) => (
+              <ul className="list-inside list-disc">{props.children}</ul>
+            ),
+            pre: (props) => <pre className="mt-2 mb-2">{props.children}</pre>,
+          }}
+          source={product.descriptionMarkdown}
+        />
+      </div>
     </div>
   );
 };
